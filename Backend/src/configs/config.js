e9b34d2053b03dde6836b6295fb30b235219073f
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-11-25 01:01:57 +0530

    minor changes

--- Backend/src/configs/config.js
+++ Backend/src/configs/config.js
@@ -1,15 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import fs from 'fs';
-//import googleOauthConfig from "./googleOauthConfig.json" assert { type: "json" };
-const googleOauthConfig = JSON.parse(fs.readFileSync('/etc/secrets/googleOauthConfig.json').toString())
 
 export default {
     baseUrl: process.env.BASE_URL,
     api_version: process.env.API_VERSION,
     google: {
-        client_id: googleOauthConfig.web.client_id,
-        client_secret: googleOauthConfig.web.client_secret,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: `${process.env.BASE_URL}/api/${process.env.API_VERSION}/auth/google/callback`
     },
     mongoDB: {
